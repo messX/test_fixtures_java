b082cbfe9bc4a006596f3effd6a8a123534e46d1
messX <dhandedhan@gmail.com>
2020-08-03 22:47:48 +0530

    base template for java projects

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+@Slf4j
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        log.info("Testing ...");
+        assertTrue( true );
+    }
+}
